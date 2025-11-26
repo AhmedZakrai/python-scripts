9cbf47ec0989045fc3712e504f6578745b917ae0
ahmed <ahmedzakia511@gmail.com>
2025-11-26 06:14:24 -0500

    Scrpit - 1 - : Say Hello

---
+++ hello.py
@@ -0,0 +1,10 @@
+# This program says hello and asks for my name.
+
+print ("Hello, world!")
+nam = input("What is your name?\n")
+print (f'It is good to meet you, {nam}')
+print ("The length of your name is:")
+print (len(nam))
+age = input("What is your age?\n")
+print (f'You will be {age} in a year.')
+
